d448be348867796580b8293639edce281f6caced
Timothy Keesman <tim.keesman@gmail.com>
2021-05-14 21:23:41 +1000

    Working

--- API/edge_device_sample.py
+++ API/edge_device_sample.py
@@ -5,31 +5,20 @@ import paho.mqtt.publish as publish
 from time import sleep
 import threading
 import sys
+from helper_functions import *
 
 from BiDirectionalMQTTComms import * 
 
 class CommunicationInterface():
-    def __init__(self, topics):
+    def __init__(self, device_type, topics):
         self.ftopic_list = topics
+        self.fdevice_type = device_type
 
     def getTopicList(self):
         return self.ftopic_list
     
     def onMessage(self, topic, payload):
         print(topic + "|" + payload)
-
-#Source: https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP 
 
 global server_ip_address
 
