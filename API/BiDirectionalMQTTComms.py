302b6fa4314873c9b0217d151a57803fd6ef0b36
Timothy Keesman <tim.keesman@gmail.com>
2021-05-15 22:15:44 +1000

    Fixes

--- API/BiDirectionalMQTTComms.py
+++ API/BiDirectionalMQTTComms.py
@@ -28,14 +28,19 @@ class MQTTConnectInitializer(threading.Thread):
         
     def run(self):
         while True:
-            if self.fmqtt_bi_comms.getDeviceStatus() == ConnectionStatus.init:
-                self.fmqtt_bi_comms.fdevice_status = ConnectionStatus.attempting_connection
-                self.fmqtt_bi_comms.sendMsg("broadcast", "/edge_device/setup_device")
-            elif self.fmqtt_bi_comms.getDeviceStatus() == ConnectionStatus.attempting_connection:
-                self.fmqtt_bi_comms.sendMsg("initial message", "/edge_device/setup_device")
-            else:
-                exit()
+            try:
+                self.fmqtt_bi_comms.fconnection_setup_lock.acquire()
+            finally:
+                if self.fmqtt_bi_comms.getDeviceStatus() == ConnectionStatus.init:
+                    self.fmqtt_bi_comms.fdevice_status = ConnectionStatus.attempting_connection
+                    self.fmqtt_bi_comms.sendMsg("broadcast", "/edge_device/setup_device")
+                elif self.fmqtt_bi_comms.getDeviceStatus() == ConnectionStatus.attempting_connection:
+                    self.fmqtt_bi_comms.sendMsg("initial message", "/edge_device/setup_device")
+                else:
+                    self.fmqtt_bi_comms.fconnection_setup_lock.release()
+                    exit()
 
+                self.fmqtt_bi_comms.fconnection_setup_lock.release()
             sleep(1)
 
 class BiDirectionalMQTTComms:
