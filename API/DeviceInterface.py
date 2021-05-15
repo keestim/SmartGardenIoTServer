6bbafcb6c646ca9e9d034f717394d6bbd4a4dca0
Timothy Keesman <tim.keesman@gmail.com>
2021-05-15 16:37:23 +1000

    ID Tracking Attempt

--- API/DeviceInterface.py
+++ API/DeviceInterface.py
@@ -3,6 +3,8 @@ import abc
 class DeviceInterface():
     def __init__(self, topics = ""):
         self.ftopic_list = topics
+        self.fDeviceType = ""
+        self.ftype_id = 0
 
     def getTopicList(self):
         return self.ftopic_list
@@ -18,21 +20,34 @@ class DeviceInterface():
         return output_msg
 
 class PlantMonitorInterface(DeviceInterface):
+    global num_plant_devices
+
     def __init__(self): 
         super().__init__()
+        
         self.fTemperature = 0
         self.fMoisture = 0
         self.fHumidity = 0
+        self.fDeviceType = "PlantMonitor"
+
+        num_plant_devices = num_plant_devices + 1
+        self.ftype_id = num_plant_devices
 
     def onMessage(self, topic, payload):
         print(topic + "|" + payload)
 
 class WaterSystemInterface(DeviceInterface):
+    global num_watering_devices
+
     def __init__(self):
         super().__init__()
 
         self.fWaterVolume = 0
         self.fValueOpen = False
+        self.fDeviceType = "WaterSystem"
+
+        num_watering_devices = num_watering_devices + 1
+        self.ftype_id = num_watering_devices
 
     def onMessage(self, topic, payload):
         print(topic + "|" + payload)
