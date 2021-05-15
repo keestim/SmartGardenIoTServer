1e01ffa27459b5917a1e058f29a7d5b16b469f5a
Thomas-Bibby <62004466+Thomas-Bibby@users.noreply.github.com>
2021-05-15 17:19:35 +1000

    Updated Mains Connection
    Updated the code on the RPI

--- Mains Connection/MainsRPI.py
+++ Mains Connection/MainsRPI.py
@@ -17,14 +17,13 @@ valvePos = 0
 #MQTT
 
 topic = "/edge_device/mainsConnection"
-ipAddress = 192.168.20.167 #Ip address of the main edge device
+ipAddress = "192.168.20.167" #Ip address of the main edge device
 
 def on_connect(client, userdata, flags, rc):
-print("Connected with result code "+str(rc))
-
+#print("Connected with result code "+str(rc))
 client.subscribe(topic)
 
-def on_message(client, userdata, msg)
+def on_message(client, userdata, msg):
 arduino.write(msg.payload)
 print(msg.topic + " " + str(msg.payload))
 
@@ -33,13 +32,12 @@ print(msg.topic + " " + str(msg.payload))
 while True:
 flowrate, valvePos = map(float, arduino.readline().split())
 
-publish.single(topic, flowrate + " " + valvePos, hostname=ipAddress) # Sending arduino code to main edge server
+publish.single(topic, flowrate + " " + valvePos, hostname="192.168.20.36") # Sending arduino code to main edge server
 
-client = mqtt.Client()
+time.sleep(1)
+
+client = mqtt.Client("edgedeviceMains")
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.connect(ipAddress, 1883, 60)
-#client.loop_forever()
-
-##time.sleep(1)
+client.loop_start()
