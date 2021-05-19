d02604de139189bfce1337bba990812818cb67fd
Thomas-Bibby <62004466+Thomas-Bibby@users.noreply.github.com>
2021-05-19 20:48:22 +1000

    Update loaddevices.js

--- Website/scripts/loaddevices.js
+++ Website/scripts/loaddevices.js
@@ -1,4 +1,8 @@
-$(document).ready(function (e)
-{
-  $("#devicesRaw").load("<p>" + "/http://localhost/xampp/SmartGardenIoTServer/Website/test.html" + "</p>");
-});
+function reqListener () {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "127.0.0.1:5000/probe_devices");
+oReq.send();
