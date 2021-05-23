258ca633eabb34161472cb5074817c35c2a86902
Thomas-Bibby <62004466+Thomas-Bibby@users.noreply.github.com>
2021-05-23 22:24:54 +1000

    Thomas Arduino code final

--- Mains Connection/Thomas_arduino/ArduinoWateringEdgeDevice/ArduinoWateringEdgeDevice.ino
+++ Mains Connection/Thomas_arduino/ArduinoWateringEdgeDevice/ArduinoWateringEdgeDevice.ino
@@ -57,45 +57,28 @@ void initialiseFlowMeter()
   totalMilliLitres  = 0;
   oldTime           = 0;
   
-  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
-  // Configured to trigger on a FALLING state change (transition from HIGH
-  // state to LOW state)
   attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
 }
 
 void readFlowMeter(){
-  // only record is valve is open
-  //when valve is closed, reset water measurement, etc!
-  if((millis() - oldTime) > 1000)    // Only process counters once per second
+
+  if((millis() - oldTime) > 1000)    
   { 
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
+
     detachInterrupt(sensorInterrupt);
         
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
+
     flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
     
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
     oldTime = millis();
     
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
     flowMilliLitres = (flowRate / 60) * 1000;
-    
-    // Add the millilitres passed in this second to the cumulative total
+  
     totalMilliLitres += flowMilliLitres;
       
     unsigned int frac;
 
-    // Reset the pulse counter so we can start incrementing again
+    // Reset pulse 
     pulseCount = 0;
     
     // Enable the interrupt again now that we've finished sending output
@@ -103,7 +86,6 @@ void readFlowMeter(){
   }
 }
 
-//when every 0.5 seconds quickly close and open valve, to prevent any over pour issues, etc
 void loop() 
 {
   readSerialMsgs();
@@ -137,8 +119,6 @@ void readSerialMsgs()
     }
   }
 
-  // if the string that's been read in is longer that 0 length, then try to parse the string as JSON
-  // all serial communication should occur through JSON
   if (serialMsg.length() > 0)
   {
     DynamicJsonDocument doc(200);    
@@ -165,9 +145,7 @@ void readSerialMsgs()
   }
 }
 
-/*
-Insterrupt Service Routine
- */
+
 void pulseCounter()
 {
   // Increment the pulse counter
